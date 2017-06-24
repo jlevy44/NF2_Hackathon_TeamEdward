119fe8393ac9e12bc39b76c9fbdfc6ef042c6d6a
jlevy44 <joshualevy44@berkeley.edu>
2017-06-24 10:57:13 -0700

    mod SNP prog

--- Scripts/SNPDensity.py
+++ Scripts/SNPDensity.py
@@ -1,6 +1,7 @@
 from pybedtools import BedTool
 import numpy as np
 import matplotlib.pyplot as plt
+from collections import defaultdict
 
 with open('/mnt/disks/data-bam-tumor/Homo_sapiens_assembly19.fasta.fai','r') as f:
     chromosomes = {line.split('\t')[0]:int(line.split('\t')[1]) for line in f.readlines()[0:24]}
@@ -25,3 +26,15 @@ print b.head()
 a.coverage(b).saveas('VCFCoverage.bed')#,hist=True
 
 #for k in chromosomes.keys():
+positionHistogram = defaultdict(list)
+
+with open('/mnt/disks/data-vcf/GSN79Tumor_normal.vcf','r') as f:
+    for line in f.readlines():
+        if line and line.startswith('#') ==0:
+            if line.split('\t')[0] not in positionHistogram.keys():
+                positionHistogram[line.split('\t')[0]] = []
+            positionHistogram[line.split('\t')[0]].append(int(line.split('\t')[1]))
+
+for k in chromosomes.keys():
+    plt.hist(positionHistogram[k],bins=chrom_Intervals[k])
+    plt.savefig(k+'SNPDense.png')
