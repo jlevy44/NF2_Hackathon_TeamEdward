35b5d2f8cd9d52b35a6886e38a26289096d5e967
jlevy44 <joshualevy44@berkeley.edu>
2017-06-24 10:31:05 -0700

    mod SNP prog

--- Scripts/SNPDensity.py
+++ Scripts/SNPDensity.py
@@ -3,12 +3,14 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 with open('/mnt/disks/data-bam-tumor/Homo_sapiens_assembly19.fasta.fai','r') as f:
-    chromosomes = {line.split('\t')[0]:int(line.split('\t')[1]) for line in f.readlines()[0:25]}
+    chromosomes = {line.split('\t')[0]:int(line.split('\t')[1]) for line in f.readlines()[0:24]}
 
 print chromosomes
 
 chrom_Intervals = {k:np.append(np.arange(0,chromosomes[k],10000),chromosomes[k]) for k in chromosomes.keys()}
 
+for k in chromosomes.keys():
+    print chrom_Intervals[k][0:10]
 
 with open('humanBedInterval.bed','w') as f:
     for k in chromosomes.keys():
