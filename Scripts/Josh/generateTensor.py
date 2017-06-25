3c04d26e781e4b4fe51e0c0132d8beae2cc8d148
jlevy44 <joshualevy44@berkeley.edu>
2017-06-25 12:51:07 -0700

    Dataset creation for sci-kit learn

--- Scripts/Josh/generateTensor.py
+++ Scripts/Josh/generateTensor.py
@@ -21,7 +21,7 @@ def genDataset(genes,testTrain): # second argument is test or train bed dictiona
             #indelTest = testTrain['indel'].intersect(geneBed,wa=True)
             #snpDensity = []
             #indelDensity = []
-            print [np.arange(interval[0],interval[1],100),np.arange(interval[0]+50,interval[1],100)]
+            #print [np.arange(interval[0],interval[1],100),np.arange(interval[0]+50,interval[1],100)]
             for bin in [np.arange(interval[0],interval[1],100),np.arange(interval[0]+50,interval[1],100)]:
                 for i in range(len(bin)-1):
                     interval = bin[i:i+2]
